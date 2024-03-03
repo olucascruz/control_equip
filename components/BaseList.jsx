1ce1e938ac264aad259917801ce5da668c7e2fcf
LucasCruz <lca.snf21@uea.edu.br>
2024-03-03 11:29:27 -0400

    fix list bug

--- components/BaseList.jsx
+++ components/BaseList.jsx
@@ -1,15 +1,27 @@
 import { View, Text, StyleSheet} from "react-native"
 
-export default function BaseList({listItems}){
+export default function BaseList({listItems, showKey=false}){
+    const getContent = (object) =>{
+        const keys = Object.keys(object)
+        let content = ''
+        keys.forEach(key => {
+            if(showKey){
+                content += `${key}: ${object[key]} \n`
+            }else{
+                content += `${object[key]} \n`
+            }
+        });
+        return content
+    }
+
     return(
         <View style={styles.container}>
             {
             listItems.map((item, index)=>{
                 return(
                 <View key={index} style={styles.itemList}>
-                    <Text>{item}</Text>
+                    <Text>{getContent(item)}</Text>
                 </View>)    
-            
             })
             }
         </View>
@@ -22,15 +34,22 @@ const styles = StyleSheet.create({
         marginTop:20,
         display:"flex",
         backgroundColor:"#fff",
-        width:"70%",
+        width:"100%",
         alignItems:"center",
     },
-    itemList:{
-        display:"flex",
-        borderWidth: 2, // Define a largura da borda
-        borderColor: 'black', // Define a cor da borda
-        width:"100%",    
-        alignItems:"center",
+    itemList: {
+        display: "flex",
+        borderWidth: 1,
+        width: "100%",
+        height:"auto",
+        alignItems: "center",
+        borderTopWidth: 0, // Define apenas a largura da borda superior
+        borderLeftWidth: 0, // Remove a borda esquerda
+        borderRightWidth: 0, // Remove a borda direita
+        paddingVertical: 10, // Adiciona espaço interno vertical
+        paddingHorizontal: 20, // Adiciona espaço interno horizontal
+        borderColor: 'gray',
+        marginVertical: 5,
     }
 
 })
