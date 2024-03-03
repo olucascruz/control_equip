0f2134567a39f7ff62358a11f5b7201b7251b4b0
LucasCruz <lca.snf21@uea.edu.br>
2024-03-03 11:30:14 -0400

    feat: add scroll

--- components/BaseView.jsx
+++ components/BaseView.jsx
@@ -1,18 +1,21 @@
-import { View, StyleSheet } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 
 
 export default function BaseView({children}){
 
     return(
+        <ScrollView>
             <View style={styles.container}>
              {children}
             </View>
+        </ScrollView>
         )
 }
 
 
 const styles = StyleSheet.create({
     container:{
+        height:800,
         flex:1,
         backgroundColor: "#ececec",
         alignItems:"center"  
