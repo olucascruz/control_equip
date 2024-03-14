2213ec6bc9449b7dc9c1234a26c32e8b780f6b2e
LucasCruz <lca.snf21@uea.edu.br>
2024-03-14 15:34:27 -0400

    refactor: modal updated

--- src/components/ModalEditDelete.jsx
+++ src/components/ModalEditDelete.jsx
@@ -2,26 +2,114 @@ import { useState } from "react"
 import { View,  Text, Button, Modal, StyleSheet } from "react-native"
 import { textStyles } from "./TextStyles"
 import { buttonStyled, colorAddButton } from "./ButtonStyled";
-export default function ModalEditDelete({modalVisible, setModalVisible, itemSelected, setItemSelected, Inputs, editFunc, deleteFunc}){
+export default function ModalEditDelete({
+    modalVisible, 
+    setModalVisible, 
+    itemSelected, 
+    setItemSelected, 
+    Inputs, 
+    editFunc =null, 
+    deleteFunc=null,
+    cleanCallback=null,
+    }){
+    
     // wait, delete, edit, finish
     const [stateModal, setStateModal] = useState("wait")
+    
     const hiddenModal = () => {
         setModalVisible(false);
         setStateModal("wait")
         setItemSelected(null)
+        if(cleanCallback) cleanCallback()
     };
 
     const handleSubject = (operations) =>{
-        if(operations=="delete")
+        if(operations=="delete" && deleteFunc)
         {
         deleteFunc()
+        setStateModal("finish")
+
         }
-        if(operations=="edit")
+        if(operations=="edit" && editFunc)
         {
-        editFunc()
+        editFunc(setStateModal)
         }
 
-        setStateModal("finish")
+    }
+    const waitState = ()=>{
+        return (
+            <>
+                <Text>{itemSelected ? itemSelected.content:null}</Text>
+                <View style={styles.modalButtonsView}>
+                    {editFunc ? <View style={buttonStyled.container}>
+                        <Button onPress={()=>{setStateModal("edit")}} color={colorAddButton} title="Editar"/>
+                    </View>  : null}
+                    {deleteFunc ? <View style={buttonStyled.container}>
+                        <Button onPress={()=>{setStateModal("delete")}} color={colorAddButton} title="Deletar"/>
+                    </View> :null}
+                    
+                    <View style={buttonStyled.container}>
+                        <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
+                    </View>
+                </View>
+            </>
+            )
+    }
+    const editState = ()=>{
+        return (
+            <>
+                <Text style={textStyles.label}>Edite a disciplina:</Text>
+                
+                {Inputs ? Inputs():null}
+                <View style={buttonStyled.container}>
+                    <Button onPress={()=>{handleSubject("edit")}} color={colorAddButton} title="Editar"/>
+                </View>
+                <View style={buttonStyled.container}>
+                    <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
+                </View>
+            </>
+            )
+    }
+    const deleteState = () =>{
+        return (
+            <>
+                <Text style={textStyles.label}>Tem certeza que seja deletar?</Text>
+                <View style={buttonStyled.container}>
+                    <Button onPress={()=>{handleSubject("delete")}} color={colorAddButton} title="Confirmar"/>
+                </View>
+                <View style={buttonStyled.container}>
+                    <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
+                </View>
+            </>
+            )
+    }
+    const finishState = ()=>{
+        return (
+            <>
+               <Text style={textStyles.label}>{itemSelected ?itemSelected.feedback:"..."}</Text>
+
+               <View style={buttonStyled.container}>
+                <Button onPress={hiddenModal} color={colorAddButton} title="OK "/>
+            </View>
+            </>
+        )
+    }
+    const switchModalState = () => {
+        switch (stateModal) {
+            case "wait":
+                return waitState()
+            case "edit":
+                return editState()
+
+            case "delete":
+                return deleteState()
+
+            case "finish":
+                return finishState()
+            
+            default:
+                return waitState()
+        }
     }
 
     return(
@@ -33,62 +121,11 @@ export default function ModalEditDelete({modalVisible, setModalVisible, itemSele
         >
             <View style={styles.modalContainer}>
                 <View style={styles.modalContent}>
-                    {stateModal === "wait" ? (
-                    <>
-                        <Text>{itemSelected ? itemSelected.content:null}</Text>
-                        <View style={styles.modalButtonsView}>
-                            <View style={buttonStyled.container}>
-                                <Button onPress={()=>{setStateModal("edit")}} color={colorAddButton} title="Editar"/>
-                            </View>
-                            <View style={buttonStyled.container}>
-                                <Button onPress={()=>{setStateModal("delete")}} color={colorAddButton} title="Deletar"/>
-                            </View>
-                            <View style={buttonStyled.container}>
-                                <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
-                            </View>
-                        </View>
-                    </>
-                    ) : stateModal === "edit" ? (
-                    <>
-                        <Text style={textStyles.label}>Edite a disciplina:</Text>
-                        
-                        {Inputs()}
-                        <View style={buttonStyled.container}>
-                            <Button onPress={()=>{handleSubject("edit")}} color={colorAddButton} title="Editar"/>
-                        </View>
-                        <View style={buttonStyled.container}>
-                            <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
-                        </View>
-                    </>
-                    ): stateModal === "delete" ?(
-                    <>
-                        <Text style={textStyles.label}>Tem certeza que seja deletar?</Text>
-                        <View style={buttonStyled.container}>
-                            <Button onPress={()=>{handleSubject("delete")}} color={colorAddButton} title="Confirmar"/>
-                        </View>
-                        <View style={buttonStyled.container}>
-                            <Button onPress={hiddenModal} color={colorAddButton} title="Cancelar"/>
-                        </View>
-                    </>
-                    ): stateModal === "finish" ? (
-                        <>
-                           <Text style={textStyles.label}>{itemSelected ?itemSelected.feedback:null}</Text>
-
-                           <View style={buttonStyled.container}>
-                            <Button onPress={hiddenModal} color={colorAddButton} title="OK "/>
-                        </View>
-                        </>
-                        ) : null
-                
-                }
-                            
+                    {switchModalState()}
                 </View>
             </View>
         </Modal>
-
         )
-
-
 }
 
 
