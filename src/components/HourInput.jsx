ed3da5ae45d492209cc6bf6cedf081991e559513
LucasCruz <lca.snf21@uea.edu.br>
2024-03-14 14:08:30 -0400

    feat: value = state, and keyboardType = numeric

--- src/components/HourInput.jsx
+++ src/components/HourInput.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { TextInputMask } from 'react-native-masked-text';
 import { View, StyleSheet } from 'react-native';
 
-export default function HourInput({ onValueChange, placeholder }){
+export default function HourInput({value, onValueChange, placeholder }){
     const handleChange = (text) => {
         onValueChange(text);
       };
     
     return (
       <TextInputMask
+        value={value}
         style={styles.input}
         type={'custom'}
         options={{
           mask: '99:99'
         }}
+        keyboardType="numeric"
         onChangeText={handleChange}
         placeholder={placeholder || 'HH:MM'}
       />
