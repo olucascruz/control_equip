45fb44e3e9be310236952ad3fd181ca720ceb006
LucasCruz <lca.snf21@uea.edu.br>
2024-03-14 02:17:17 -0400

    refactor: change component to styled

---
+++ src/components/InputStyled.jsx
@@ -0,0 +1,15 @@
+
+import { StyleSheet } from "react-native"
+
+
+export const inputStyled = StyleSheet.create({
+    input:{
+        backgroundColor:"#fff",
+        width: "70%",
+        height:50,
+        borderRadius:5,
+        marginTop:20,
+        marginBottom:20,
+        paddingLeft:10,
+    },
+})
