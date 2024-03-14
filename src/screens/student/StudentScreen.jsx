abe0b3bcd83a25349eadfdc54bccaaaac7ce6f4f
LucasCruz <lca.snf21@uea.edu.br>
2024-03-14 17:35:14 -0400

    feat: add modal to delete student

--- src/screens/student/StudentScreen.jsx
+++ src/screens/student/StudentScreen.jsx
@@ -1,14 +1,24 @@
-import { View, Text, Button } from "react-native"
+// Core react
+import { View, Text, Button, TextInput} from "react-native"
 import { useState, useContext } from "react"
+
+// Components
 import Header from "../../components/Header"
-import BaseInput from "../../components/BaseInput"
 import BaseList from "../../components/BaseList"
 import SubjectPicker from "../../components/SubjectPicker"
 import BaseView from "../../components/BaseView"
+import ModalEditDelete from "../../components/ModalEditDelete"
+
+//Styled
+import { inputStyled } from "../../components/InputStyled"
 import { textStyles } from "../../components/TextStyles";
 import { buttonStyled, colorAddButton } from "../../components/ButtonStyled";
+
+//Context
 import { dataContext } from "../../contexts/Data"
-import { addStudent } from "../../storage/studentRepository"
+
+// Storage
+import { addStudent, getStudents, deleteStudent } from "../../storage/studentRepository"
 import { addStudentToSubject } from "../../storage/studentSubjectRepository"
 
 
@@ -17,7 +27,8 @@ export default function StudentScreen(){
     const [valueInputNameStudent, setValueInputNameStudent] = useState(null)
     const [valueInputCodeStudent, setValueInputCodeStudent] = useState(null)
     const [valueInputSubject, setValueInputSubject] = useState(null)
-
+    const [modalVisible, setModalVisible] = useState(false);
+    const [itemSelected, setItemSelected] = useState("")
 
     
     const addStudentHandler = () =>{
@@ -26,37 +37,71 @@ export default function StudentScreen(){
             !valueInputCodeStudent ) return 
 
         const newStudent = {
-            "id": valueInputNameStudent,
-            "name": valueInputCodeStudent
+            "id": valueInputCodeStudent,
+            "name": valueInputNameStudent
         }
-        addStudent(database,valueInputCodeStudent, valueInputNameStudent)
-        setTimeout(() => {
-            addStudentToSubject(database, valueInputCodeStudent, valueInputSubject)
-        }, 300);
-        setListStudent([...listStudent, newStudent])
+        addStudent(database, valueInputCodeStudent, valueInputNameStudent,
+            ()=>{
+                addStudentToSubject(database, valueInputCodeStudent, valueInputSubject)
+                setListStudent([...listStudent, newStudent])    
+            })
     
     }
 
+    const listStudentFormatted = listStudent.map(student=>{
+        const content = `${student.name} \nMatrícula: ${student.id}`
+        return content
+    })
+    const ids = listStudent.map(student=>{ return student.id})
+
+    const onClickItemList = (itemSelected) => {
+        setModalVisible(true)
+        setItemSelected(itemSelected)
+    }
+
+
+    const handleDeleteStudent = () => {
+        deleteStudent(database, itemSelected.id, ()=>{
+            itemSelected["feedback"] = "item deletado com sucesso"
+            setItemSelected(itemSelected)
+            getStudents(database, students =>{
+                setListStudent(students)
+           })   
+        })
+    
+    }
     return(
         <BaseView>
+            <ModalEditDelete
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            itemSelected={itemSelected}
+            setItemSelected={setItemSelected}
+            deleteFunc={handleDeleteStudent}
+            />
             <Header headerTitle={"Estudantes"}/>
             <Text style={textStyles.label}>Disciplina:</Text>
             <SubjectPicker disciplines={listSubject}  selectedHandler={setValueInputSubject}/>
             <Text style={textStyles.label}>Nome do estudante:</Text>
-            <BaseInput 
-            onValueChange={setValueInputNameStudent}
+            <TextInput style={inputStyled.input} 
+            onChangeText={setValueInputNameStudent}
             placeholder={"Nome do estudante"}/>
 
-            <Text style={textStyles.label}>Código do estudante:</Text>
-            <BaseInput 
-            onValueChange={setValueInputCodeStudent}
-            placeholder={"Código do estudante"}/>
+            <Text style={textStyles.label}>Número de matrícula:</Text>
+            <TextInput style={inputStyled.input}
+            onChangeText={setValueInputCodeStudent}
+            placeholder={"Número de matrícula"}
+            keyboardType="numeric"/>
+            
             <View style={buttonStyled.container} >
                 <Button onPress={addStudentHandler}
                         color={colorAddButton}
                         title="adicionar estudante"/>
             </View>
-            <BaseList listItems={listStudent} showKey={true}/>
-        </BaseView>
+            <BaseList 
+            listItems={listStudentFormatted} 
+            ids={ids} 
+            customFunc={onClickItemList}/>
+            </BaseView>
     )
 }
