00850f8dbc7e42775b1df4214d9897c108f5afbb
LucasCruz <lca.snf21@uea.edu.br>
2024-03-18 17:57:01 -0400

    feat: add callback and get

--- src/storage/studentSubjectRepository.js
+++ src/storage/studentSubjectRepository.js
@@ -20,11 +20,28 @@ export function addStudentToSubject(db, studentId, subjectId) {
 }
 
 // Função para obter todos os estudantes da tabela Student
-export function getStudentsBySubject(db, callback, subjectId) {
+export function getStudentsBySubject(db, subjectId, callback=null) {
     db.transaction(tx => {
         tx.executeSql(
             'SELECT student FROM Student_Subject WHERE subject = ?',
             [subjectId],
+            (tx, results) => {
+                console.log('Consulta executada com sucesso');
+                const result = results.rows._array;
+                callback(result);
+            },
+            error => {
+                console.error('Erro ao obter os estudantes:', error);
+            }
+        );
+    });
+}
+
+export function getStudentSubject(db, callback) {
+    db.transaction(tx => {
+        tx.executeSql(
+            'SELECT * FROM Student_Subject',
+            [],
             (tx, results) => {
                 console.log('Consulta executada com sucesso');
                 const result = results.rows._array;
