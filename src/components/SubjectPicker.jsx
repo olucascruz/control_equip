c172283f6d927271ee857af9eaea8a6f19b1251a
LucasCruz <lca.snf21@uea.edu.br>
2024-03-20 14:04:44 -0400

    fix: set state subject fixed

--- src/components/SubjectPicker.jsx
+++ src/components/SubjectPicker.jsx
@@ -2,22 +2,20 @@ import { Picker } from "@react-native-picker/picker"
 import { StyleSheet } from "react-native";
 import { useState } from "react";
 
-export default function SubjectPicker({disciplines, selectedHandler}){
-    const [selectedValue, setSelectedValue] = useState('java');
+export default function SubjectPicker({disciplines, selectedHandler, selectedValue}){
     const selectedValueHandler = (itemValue) =>{
-        setSelectedValue(itemValue)
         selectedHandler(itemValue)
     }
     return (
         <Picker
             selectedValue={selectedValue}
-            onValueChange={(itemValue, itemIndex) => selectedValueHandler(itemValue)}
+            onValueChange={itemValue => selectedValueHandler(itemValue)}
             style={styles.picker}
             >
             {disciplines ? disciplines.map((discipline, index)=>{
                 return <Picker.Item key={`${index}_${discipline}`} 
                 label={discipline.name } 
-                value={discipline.name} />
+                value={discipline} />
             }): null}
             </Picker>
             )
